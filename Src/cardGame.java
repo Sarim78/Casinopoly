726d2386e99980f2ea8ed9a2d89aa5b6e1350355
Sarim <74938278+Sarim78@users.noreply.github.com>
2022-12-16 10:03:24 -0500

    Created Slot and cardGame File

---
+++ Src/cardGame.java
@@ -0,0 +1,16 @@
+package Src;
+import java.util.Scanner;
+import java.util.Random;
+import java.util.ArrayList;
+
+/**
+ * Card Game (Hi-Lo)
+ * @Athuor Sarim Siddiqui
+ */
+
+public class cardGame {
+
+    // Creating Objects 
+    Random rnd = new Random();
+    Scanner in = new Scanner(System.in);
+}
