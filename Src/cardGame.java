5d0c3e540ca3e6776ca601efb552dd1e0c7e18c5
Sarim <74938278+Sarim78@users.noreply.github.com>
2022-12-21 15:28:32 -0500

    still working...

--- Src/cardGame.java
+++ Src/cardGame.java
@@ -14,9 +14,11 @@ public class cardGame {
     static Scanner in = new Scanner(System.in);
 
     // Creating Variables
-    static int numberLimit = 25000;
+    static int numberLimit = 15000;
 
     static int num;
+
+    static int ai;
 
     static String Hi;
     static String Lo; 
@@ -26,17 +28,37 @@ public class cardGame {
     public static void numberGenerator() {
         // This methid will tell the player, if you need to guess Hi or Lo
         int num = rnd.nextInt(numberLimit);
-        System.out.println("The Number, You Have To Guess Hi Or Lo Is: " + num);
+        System.out.println("\nThe Number Is: " + num);
+    }
+
+    public static void Ai() {
+        // this method will let the ai think of a number greater than or less then numberGenerator() method
+        System.out.println("\nAi: Am i thinking of an number higher or Lower?");
+
+        int aiNum = rnd.nextInt(numberLimit);
+        ai = aiNum;
+
+        if(aiNum > num) {
+            System.out.println("ai test 1");
+        } else if(aiNum < num) {
+            System.out.println("ai test 2");
+        } else { 
+            System.out.println("ai test 3");
+        }
     }
 
     public static void HiLo() {
+
+        numberGenerator();
+        Ai();
+
         // Player guess if the number is Hi or Lo
         System.out.println("Please Enter Your Guess (Hi/Lo): ");
-        int userInput = in.nextInt();
+        String userInput = in.nextLine();
 
-        if(userInput < num) {
-            System.out.println("Test 1"); 
-        } else if (userInput > num) {
+        if(userInput.equals("Hi") || userInput.equals("hi")) {
+            System.out.println("Test 1");
+        } else if (userInput.equals("Lo") || userInput.equals("lo")) {
             System.out.println("Test 2"); 
         } else {
             System.out.println("Test 3"); 
