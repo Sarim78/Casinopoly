f17356cfffe4f13dca2debbf0ffa3dfbce9dec71
Sarim <74938278+Sarim78@users.noreply.github.com>
2022-12-28 22:38:45 -0500

    Created Array

--- game/gameBoard.java
+++ game/gameBoard.java
@@ -20,6 +20,9 @@ public class gameBoard {
     // Creating Variables
     static int totalSpace;
 
+    // Array
+    int[] spaces = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18}; 
+
     // Methods 
     public static void dice() {
         int dice = rnd.nextInt(1, 6);
