922552e929d2652266f2ae92838faac5aca9fbfc
Adil <107200522+Proftblculture@users.noreply.github.com>
2023-01-17 21:17:46 -0500

    Update casinopolyMain.java

--- src/casinopolyMain.java
+++ src/casinopolyMain.java
@@ -20,9 +20,6 @@ import game.umbrella;
 public class casinopolyMain {
     public static void main(String[] args) {
 
-        // casinopolyShop casinopolyShop = new casinopolyShop();
-        // casinopolyShop.casinopolyshop();
-
         casinopolyShop casinopolyShop = new casinopolyShop();
         //casinopolyShop.casinopolyshop();
 
